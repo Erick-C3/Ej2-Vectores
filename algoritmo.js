4a0c804575c05aa39bf91197ef9e1f67f89f6f23
Erick <erick.nahuelpaiva.001@gmail.com>
2023-05-30 11:34:27 -0300

    modularizado

--- algoritmo.js
+++ algoritmo.js
@@ -11,19 +11,36 @@ const cantidadesProd = [CANT_INICIAL_PROD, CANT_INICIAL_PROD, CANT_INICIAL_PROD]
 
 function comprar(){
     /* console.log(  cantidadesProd[  selectOpcProdCompra.value  ] -= CANT_UNIDADES_COMPRA  ); */
-    
     //resta el stock del producto correspondiente en -CANT_UNIDADES_COMPRA 
-    cantidadesProd[  selectOpcProdCompra.value  ] -= CANT_UNIDADES_COMPRA;
+    actualizarCantidadProdComprar();
 
     //pequeño reto: modificar el codigo para que solamente actualice la
     // cantidad del producto correspondiente
+    actualizarInfoProductos();
+}
 
+
+
+
+
+
+/**
+ * Actualiza la cantidad del producto a comprar
+ */
+function actualizarCantidadProdComprar() {
+    cantidadesProd[selectOpcProdCompra.value] -= CANT_UNIDADES_COMPRA;
+}
+
+/**
+ * Actualiza la info de los productos en la pagina
+ */
+function actualizarInfoProductos() {
     divInfoProducto.innerHTML = `
         <span>Producto 1</span>
-        <span>Cantidad: ${cantidadesProd[0]  }</span>
+        <span>Cantidad: ${cantidadesProd[0]}</span>
         <span>Producto 2</span>
-        <span>Cantidad: ${cantidadesProd[1] }</span>
+        <span>Cantidad: ${cantidadesProd[1]}</span>
         <span>Producto 3</span>
-        <span>Cantidad: ${cantidadesProd[2] }</span>
+        <span>Cantidad: ${cantidadesProd[2]}</span>
     `;
 }
