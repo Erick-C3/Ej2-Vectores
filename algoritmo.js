313e820d99cb89b6b1e417881d61a058ee04a2aa
Erick <erick.nahuelpaiva.001@gmail.com>
2023-05-31 18:54:32 -0300

    mejora al actualizar info de producto

--- algoritmo.js
+++ algoritmo.js
@@ -1,6 +1,10 @@
 const selectOpcProdCompra = document.querySelector("select"); 
 const divInfoProducto =  document.querySelector("#info-productos");
 
+/* const spanCantidadProd1 = document.querySelector("#cant-prod-1");
+const spanCantidadProd2 = document.querySelector("#cant-prod-2");
+const spanCantidadProd3 = document.querySelector("#cant-prod-3"); */
+
 const CANT_INICIAL_PROD = 100;
 const CANT_UNIDADES_COMPRA = 1;
 
@@ -16,7 +20,7 @@ function comprar(){
 
     //pequeño reto: modificar el codigo para que solamente actualice la
     // cantidad del producto correspondiente
-    actualizarInfoProductos();
+    actualizarInfoProducto();
 }
 
 
@@ -34,13 +38,20 @@ function actualizarCantidadProdComprar() {
 /**
  * Actualiza la info de los productos en la pagina
  */
-function actualizarInfoProductos() {
-    divInfoProducto.innerHTML = `
+function actualizarInfoProducto() {
+    let posProducto = parseInt(selectOpcProdCompra.value);
+    let spanCantProdRequerido = document.querySelector(`#cant-prod-${posProducto+1}`);
+    
+    spanCantProdRequerido.innerHTML = `
+        Cantidad: ${cantidadesProd[posProducto]}
+    `;
+
+    /* divInfoProducto.innerHTML = `
         <span>Producto 1</span>
         <span>Cantidad: ${cantidadesProd[0]}</span>
         <span>Producto 2</span>
         <span>Cantidad: ${cantidadesProd[1]}</span>
         <span>Producto 3</span>
         <span>Cantidad: ${cantidadesProd[2]}</span>
-    `;
+    `; */
 }
